5a4b99ef755f0fc27fdc975cd5a2c8a6c45b72ec
HimanshuBCS <himanshu.pandey@bridgeconn.com>
2018-02-20 17:18:01 +0530

    Added i18n title message in footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -61,39 +61,70 @@ class Footer extends React.Component {
     }
     sliderFontChange(obj){
         var elements = (document.getElementsByClassName("col-ref").length - 1)
+        console.log(elements);
         for (var i = 0; i <= elements; i++) {
         document.getElementsByClassName("col-ref")[i].style.fontSize = obj.target.value + "px";
         }
     }
 
     render() {
-        // var currentValue = TodoStore.currentValue
-        // console.log(currentValue)
         return (
         <nav className="navbar navbar-default navbar-fixed-bottom">
             <div className="container-fluid">
                  <div className="collapse navbar-collapse">
                         <div style={{float:"left"}} className="btn-group navbar-btn verse-diff-on" role="group" aria-label="...">
                             <span>
-                                <a className="btn btn-default font-button minus" data-toggle="tooltip" data-placement="top" title="Decrease font size" onClick= {this.fontChange.bind(this, (-2))}>A-</a>
+                            <FormattedMessage id="tooltip-minus-font-size" >
+                            {(message) =>
+                                <a className="btn btn-default font-button minus" data-toggle="tooltip" data-placement="top" title={message} onClick= {this.fontChange.bind(this, (-2))}>A-</a>
+                            }
+                            </FormattedMessage>
                             </span>
-                                <ReactBootstrapSlider change={this.sliderFontChange.bind(this)} value={TodoStore.currentFontValue}   step={TodoStore.fontStep} max={TodoStore.fontMax} min={TodoStore.fontMin} orientation="horizontal"/>
+                                <ReactBootstrapSlider
+                                    change={this.sliderFontChange.bind(this)}
+                                    value={TodoStore.currentFontValue}
+                                    step={TodoStore.fontStep}
+                                    max={TodoStore.fontMax}
+                                    min={TodoStore.fontMin}
+                                    orientation="horizontal"
+                                />
                             <span>
-                                <a className="btn btn-default font-button plus" data-toggle="tooltip" data-placement="top" title="Increase font size" onClick= {this.fontChange.bind(this, (+2))}>A+</a>
+                            <FormattedMessage id="tooltip-plus-font-size" >
+                            {(message) =>
+                                <a className="btn btn-default font-button plus" data-toggle="tooltip" data-placement="top" title={message} onClick= {this.fontChange.bind(this, (+2))}>A+</a>
+                            }
+                            </FormattedMessage>
                             </span>
                         </div>
                     <div className="nav navbar-nav navbar-center verse-diff-on">
                         <div className="btn-group navbar-btn layout" role="group" aria-label="...">
-                            <a className="btn btn-primary btn-default" onClick = {this.handleChange.bind(this,1)} href="#" data-output="2x" role="multi-window-btn" data-toggle="tooltip" data-placement="top" title="2-column layout">2x &nbsp;<i className="fa fa-columns fa-lg"></i></a>
-                            <a className="btn btn-primary btn-default" onClick = {this.handleChange.bind(this,2)} href="#" data-output="3x" role="multi-window-btn" data-toggle="tooltip" data-placement="top" title="3-column layout">3x &nbsp;<i className="fa fa-columns fa-lg"></i>
-                            </a>
-                            <a className="btn btn-primary btn-default" onClick = {this.handleChange.bind(this,3)} href="#" data-output="4x" role="multi-window-btn" data-toggle="tooltip" data-placement="top" title="4-column layout">4x &nbsp;<i className="fa fa-columns fa-lg"></i></a>
+                            <FormattedMessage id="tooltip-2-column">
+                                { (message) =>
+                                  <a className="btn btn-primary btn-default" onClick = {this.handleChange.bind(this,1)} href="#" data-output="2x" role="multi-window-btn" data-toggle="tooltip" data-placement="top" title={message} >2x &nbsp;<i className="fa fa-columns fa-lg"></i>
+                                  </a>
+                                }
+                            </FormattedMessage>
+                            <FormattedMessage id="tooltip-3-column">
+                                { (message) =>
+                                <a className="btn btn-primary btn-default" onClick = {this.handleChange.bind(this,2)} href="#" data-output="3x" role="multi-window-btn" data-toggle="tooltip" data-placement="top" title={message}>3x &nbsp;<i className="fa fa-columns fa-lg"></i>
+                                </a>
+                                }
+                            </FormattedMessage>
+                            <FormattedMessage id="tooltip-4-column">
+                                { (message) =>                            
+                                <a className="btn btn-primary btn-default" onClick = {this.handleChange.bind(this,3)} href="#" data-output="4x" role="multi-window-btn" data-toggle="tooltip" data-placement="top" title={message}>4x &nbsp;<i className="fa fa-columns fa-lg"></i>
+                                </a>
+                            }
+                            </FormattedMessage>
                         </div>
                     </div>
-                    <span id="saved-time"></span>
+                    <span id="tooltip-btn-save"></span>
                         <ul style={{marginRight: "30px", float: "right"}} className="nav navbar-nav navbar-right">
                             <li>
-                                <a id="save-btn" data-toggle="tooltip" data-placement="top" title="Save changes" className="btn btn-success btn-save navbar-btn navbar-right" href="#" role="button" onClick={this.state.onSave}><FormattedMessage id="btn-save" /></a>
+                                <FormattedMessage id="btn-save" >
+                                {(message) =>
+                                <a id="save-btn" data-toggle="tooltip" data-placement="top" title={message} className="btn btn-success btn-save navbar-btn navbar-right" href="#" role="button" onClick={this.state.onSave}><FormattedMessage id="btn-save" /></a>}
+                                </FormattedMessage>
                             </li>
                         </ul>
                 </div>
