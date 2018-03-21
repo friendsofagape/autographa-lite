4ceaa5295ffb3dafc95f7a1cc00dcc610d92042b
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-03-21 17:17:25 +0530

    save target test case changes

--- tests/test.js
+++ tests/test.js
@@ -27,23 +27,23 @@ global.before(function() {
 
 describe('Autographa Test', () => {
 
-    beforeEach(function() {
+    before(function() {
         return app.start();
     });
 
-    afterEach(() => {
+    after(() => {
         return app.stop();
     });
 
-    // it('opens a window', () => {
-    //     return app.client.waitUntilWindowLoaded()
-    //         .getWindowCount().should.eventually.equal(1);
-    // });
+    it('opens a window', () => {
+        return app.client.waitUntilWindowLoaded()
+            .getWindowCount().should.eventually.equal(1);
+    });
 
-    // it('tests the title', () => {
-    //     return app.client.waitUntilWindowLoaded()
-    //         .getTitle().should.eventually.equal('Autographa Lite');
-    // });
+    it('tests the title', () => {
+        return app.client.waitUntilWindowLoaded()
+            .getTitle().should.eventually.equal('Autographa Lite');
+    });
  	//it("check layout", function() {
  	//        	app.client.elements('.layout2x').then((elements)=> {
  	//        	console.log(elements);
@@ -83,27 +83,34 @@ describe('Autographa Test', () => {
 	// 	return app.client.click("#btn-save");
 	// });
 
-	it('should save the target text', (done) => {
+	it('should check reference verse exist', () => {
 	 	  const input = 'this is a test';
-	 	app.client.waitUntilWindowLoaded().setValue('#v1', input)
-            .getValue("#v1")
-            .should.eventually.equal(input)
-      	app.client.click("#btn-save")
-        done();
+	 	return app.client.waitUntilWindowLoaded()
+	 		.waitForExist("#v1", 20000)
+	 		.getText("div[data-verse='r1']>.verse-num").should.eventually.equal("1")
   	});
 
-  	it('should change layout to 3x', (done) => {
-  		app.client.click("#btn-3x");
-  		done();
-  	})
-  	it('should should change layout to 4x', (done) => {
-  		app.client.click("#btn-4x");
-  		done();
-  	})
+  	it('should save the target text', () => {
+	 	  const input = 'this is a test';
+	 	return app.client.waitUntilWindowLoaded()
+	 		.waitForExist("#v1", 20000)
+	 		.setValue("#v1", input)
+	 		.waitForExist("#save-btn", 20000)
+	 		.click('#save-btn')
+	 		.getText("#v1").should.eventually.equal(input);
+  	});
 
-  	it('should should change layout to default', (done) => {
-  		app.client.click("#btn-2x");
-  		done();
+  	it('should check book button', () => {
+  		return app.client.waitUntilWindowLoaded().getText('#book-chapter-btn').should.eventually.equal('Genesis');
   	})
+  	// // it('should should change layout to 4x', (done) => {
+  	// 	app.client.click("#btn-4x").element(".layout3x");
+  	// 	done();
+  	// })
+
+  	// it('should should change layout to default', (done) => {
+  	// 	app.client.click("#btn-2x");
+  	// 	done();
+  	// })
 
 });
