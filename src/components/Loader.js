3c5361326b7a3c61f9fbd4ff63a3dfe6f33d17c4
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-04-30 15:57:49 +0530

    loader msg changes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
 class Loader extends React.Component {
 
 	render() {
 		return(
         	<div id="loading-img">
 		        <div>
-		            <p className="loading">This may take a few minutes. <br/>Please wait</p>
+					<FormattedMessage id="label-please-wait">
+                	{(message) =>
+		            <p className="loading" dangerouslySetInnerHTML={{__html: message}}></p>}
+					</FormattedMessage>
 		        </div>
     		</div>
 		)
