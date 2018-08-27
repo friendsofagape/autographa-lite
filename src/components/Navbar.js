281aa99177bd34f2914d3a905ac1f56546bc32d8
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-08-27 13:07:21 +0530

    compare mode string msg changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -491,7 +491,7 @@ class Navbar extends React.Component {
                 AutographaStore.setDiff = true
                 if(!isSameLanguage){
                     AutographaStore.toggle = false;
-                    swal(AutographaStore.currentTrans["dynamic-msg-error"], AutographaStore.currentTrans["dynamic-compare-mode"], "error");
+                    swal(AutographaStore.currentTrans["compare-mode-heading"], AutographaStore.currentTrans["dynamic-compare-mode"], "error");
                     return;
                 }
             }else{
