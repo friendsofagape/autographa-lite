ce6b33e0601c1e41a7ada98a72bce2fe1ed516c2
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-08-27 13:08:17 +0530

    search and replace empty query and radion btn selection bug resolved

--- src/components/Search.js
+++ src/components/Search.js
@@ -36,163 +36,163 @@ class SearchModal extends React.Component {
     
   constructor(props) {
     super(props);
-    this.handleFindChange = this.handleFindChange.bind(this);
-    this.handleReplaceChange = this.handleReplaceChange.bind(this);
       this.state = {
         showModalSearch: this.props.show,
         checked: false,
         replaceInfo: false,
-        replaceCount: 0,
+		replaceCount: 0,
       };
     }
 
     searchRegExp(str) {
-        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    	return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
     }
 
-    handleFindChange(event) {
-      AutographaStore.searchValue = event.target.value
+    handleFindChange = (event) => {
+    	AutographaStore.searchValue = event.target.value;
     }
 
-    handleReplaceChange(event) {
-      AutographaStore.replaceValue = event.target.value
+    handleReplaceChange = (event) => {
+    	AutographaStore.replaceValue = event.target.value;
     }
-    handleOption = (event) => {
-      AutographaStore.replaceOption = event.target.value
-    } 
+    handleOption = (value) => {
+    	AutographaStore.replaceOption = value;
+    }
 
     findAndReplaceText = (searchVal, replaceVal, option) => {
-      let that = this;
-      let allChapterReplaceCount = [];
+    	let that = this;
+    	let allChapterReplaceCount = [];
 
-      db.get(AutographaStore.bookId).then((doc) => {
-          
-              let currentBook = doc;
-              let totalReplacedWord = 0;
-              if (option == "chapter") {
-                  totalReplacedWord = that.findReplaceSearchInputs(doc.chapters[parseInt(AutographaStore.chapterId, 10) - 1].verses, AutographaStore.chapterId - 1, searchVal, replaceVal, option);
-                  allChapterReplaceCount.push(totalReplacedWord);
-              } else {
-                  for (let i = 0; i < doc.chapters.length; i++) {
-                      let replaceWord = that.findReplaceSearchInputs(doc.chapters[parseInt(i + 1, 10) - 1].verses, i, searchVal, replaceVal, option);
-                      allChapterReplaceCount.push(replaceWord)
-                      replaceWord = 0;
-                  }                  
-              }
-              var replacedCount = allChapterReplaceCount.reduce(function(a, b) {
-                    return a + b;
-              }, 0);
-              this.setState({replaceCount: replacedCount, replaceInfo: true })
-              totalReplacedWord = 0;
-              allChapterReplaceCount = [];
-          });
-        
+    	db.get(AutographaStore.bookId).then((doc) => {
+
+    		let currentBook = doc;
+    		let totalReplacedWord = 0;
+    		if (option == "chapter") {
+    			totalReplacedWord = that.findReplaceSearchInputs(doc.chapters[parseInt(AutographaStore.chapterId, 10) - 1].verses, AutographaStore.chapterId - 1, searchVal, replaceVal, option);
+    			allChapterReplaceCount.push(totalReplacedWord);
+    		} else {
+    			for (let i = 0; i < doc.chapters.length; i++) {
+    				let replaceWord = that.findReplaceSearchInputs(doc.chapters[parseInt(i + 1, 10) - 1].verses, i, searchVal, replaceVal, option);
+    				allChapterReplaceCount.push(replaceWord)
+    				replaceWord = 0;
+    			}
+    		}
+    		var replacedCount = allChapterReplaceCount.reduce(function (a, b) {
+    			return a + b;
+    		}, 0);
+    		this.setState({
+    			replaceCount: replacedCount,
+    			replaceInfo: true
+    		})
+    		totalReplacedWord = 0;
+    		allChapterReplaceCount = [];
+    	});
+
     }
 
     findReplaceSearchInputs = (verses, chapter, searchVal, replaceVal, option) => {
-      let replacedVerse = {};
-      var i;
-      let that = this;
-      let replaceCount = 0;
-      for (i = 1; i <= verses.length; i++) {
-          if (option == "chapter") {
-              let originalVerse = verses[i - 1].verse;
-              replacedVerse[i] = i;
-              if (originalVerse.search(new RegExp(that.searchRegExp(searchVal), 'g')) >= 0) {
-                  let modifiedVerse = originalVerse.replace(new RegExp(that.searchRegExp(searchVal), 'g'), replaceVal);
-                  replacedVerse[i] = modifiedVerse;
-                  chapter_hash["verse"] = modifiedVerse;
-                  chapter_hash["verse_number"] = i + 1;
-                  verses_arr.push(chapter_hash);
-                  chapter_hash = {};
-                  replaceCount += originalVerse.match(new RegExp(that.searchRegExp(searchVal), 'g')).length;
-              } else {
-                  replacedVerse[i] = originalVerse;
-                  chapter_hash["verse"] = originalVerse;
-                  chapter_hash["verse_number"] = i + 1;
-                  verses_arr.push(chapter_hash);
-                  chapter_hash = {};
-                  replaceCount += 0;
-              }
-          } else {
-              let originalVerse = verses[i - 1].verse
-              replacedVerse[i] = i;
-              if (originalVerse.search(new RegExp(that.searchRegExp(searchVal), 'g')) >= 0) {
-                  let modifiedVerse = originalVerse.replace(new RegExp(that.searchRegExp(searchVal), 'g'), replaceVal);
-                  chapter_hash["verse"] = modifiedVerse;
-                  chapter_hash["verse_number"] = i + 1;
-                  verses_arr.push(chapter_hash);
-                  chapter_hash = {};
-                  replaceCount += originalVerse.match(new RegExp(searchVal, 'g')).length;
+    	let replacedVerse = {};
+    	var i;
+    	let that = this;
+    	let replaceCount = 0;
+    	for (i = 1; i <= verses.length; i++) {
+    		if (option == "chapter") {
+    			let originalVerse = verses[i - 1].verse;
+    			replacedVerse[i] = i;
+    			if (originalVerse.search(new RegExp(that.searchRegExp(searchVal), 'g')) >= 0) {
+    				let modifiedVerse = originalVerse.replace(new RegExp(that.searchRegExp(searchVal), 'g'), replaceVal);
+    				replacedVerse[i] = modifiedVerse;
+    				chapter_hash["verse"] = modifiedVerse;
+    				chapter_hash["verse_number"] = i + 1;
+    				verses_arr.push(chapter_hash);
+    				chapter_hash = {};
+    				replaceCount += originalVerse.match(new RegExp(that.searchRegExp(searchVal), 'g')).length;
+    			} else {
+    				replacedVerse[i] = originalVerse;
+    				chapter_hash["verse"] = originalVerse;
+    				chapter_hash["verse_number"] = i + 1;
+    				verses_arr.push(chapter_hash);
+    				chapter_hash = {};
+    				replaceCount += 0;
+    			}
+    		} else {
+    			let originalVerse = verses[i - 1].verse
+    			replacedVerse[i] = i;
+    			if (originalVerse.search(new RegExp(that.searchRegExp(searchVal), 'g')) >= 0) {
+    				let modifiedVerse = originalVerse.replace(new RegExp(that.searchRegExp(searchVal), 'g'), replaceVal);
+    				chapter_hash["verse"] = modifiedVerse;
+    				chapter_hash["verse_number"] = i + 1;
+    				verses_arr.push(chapter_hash);
+    				chapter_hash = {};
+    				replaceCount += originalVerse.match(new RegExp(searchVal, 'g')).length;
 
-              } else {
-                  chapter_hash["verse"] = originalVerse;
-                  chapter_hash["verse_number"] = i + 1;
-                  verses_arr.push(chapter_hash);
-                  chapter_hash = {};
-                  replaceCount += 0;
-              }
-          }
-      }
-      replacedChapter[chapter] = replacedVerse;
-      allChapters["chapter"] = chapter + 1;
-      allChapters["verses"] = verses_arr;
-      chapter_arr.push(allChapters);
-      verses_arr = [];
-      allChapters = {};
-      // highlightRef();
-      return replaceCount;
+    			} else {
+    				chapter_hash["verse"] = originalVerse;
+    				chapter_hash["verse_number"] = i + 1;
+    				verses_arr.push(chapter_hash);
+    				chapter_hash = {};
+    				replaceCount += 0;
+    			}
+    		}
+    	}
+    	replacedChapter[chapter] = replacedVerse;
+    	allChapters["chapter"] = chapter + 1;
+    	allChapters["verses"] = verses_arr;
+    	chapter_arr.push(allChapters);
+    	verses_arr = [];
+    	allChapters = {};
+    	// highlightRef();
+    	return replaceCount;
     }
 
     saveReplacedText = () => {
-    db.get(AutographaStore.bookId).then((doc) => {
-        if (AutographaStore.replaceOption == "chapter") {
-            for (var c in replacedChapter) {
-                var verses = doc.chapters[AutographaStore.chapterId-1].verses
-                verses.forEach((verse, index)=> {
-                    verse.verse = replacedChapter[c][index + 1];
-                });
-                doc.chapters[parseInt(c, 10)].verses = verses;
-                db.put(doc, function(err, response) {
-                    if (err) {
-                        // $("#replaced-text-change").modal('toggle');
-                        // alertModal("dynamic-msg-error", "dynamic-msg-went-wrong");
-                    } else {
-                        window.location.reload();
-                    }
-                });
-            }
-            replacedChapter = {};
-            replacedVerse = {};
-        } else {
-            doc.chapters = chapter_arr
-            db.put(doc, function(err, res) {
-                if (err) {
-                    chapter_arr = [];
-                    // $("#replaced-text-change").modal('toggle');
-                    // alertModal("dynamic-msg-error", "dynamic-msg-went-wrong");
-                } else {
-                    chapter_arr = [];
-                    replacedChapter = {};
-                    replacedVerse = {};
-                    window.location.reload();
-                }
-            })
-        }
-    })
-  }
+    	db.get(AutographaStore.bookId).then((doc) => {
+    		if (AutographaStore.replaceOption == "chapter") {
+    			for (var c in replacedChapter) {
+    				var verses = doc.chapters[AutographaStore.chapterId - 1].verses
+    				verses.forEach((verse, index) => {
+    					verse.verse = replacedChapter[c][index + 1];
+    				});
+    				doc.chapters[parseInt(c, 10)].verses = verses;
+    				db.put(doc, function (err, response) {
+    					if (err) {
+    						// $("#replaced-text-change").modal('toggle');
+    						// alertModal("dynamic-msg-error", "dynamic-msg-went-wrong");
+    					} else {
+    						window.location.reload();
+    					}
+    				});
+    			}
+    			replacedChapter = {};
+    			replacedVerse = {};
+    		} else {
+    			doc.chapters = chapter_arr
+    			db.put(doc, function (err, res) {
+    				if (err) {
+    					chapter_arr = [];
+    					// $("#replaced-text-change").modal('toggle');
+    					// alertModal("dynamic-msg-error", "dynamic-msg-went-wrong");
+    				} else {
+    					chapter_arr = [];
+    					replacedChapter = {};
+    					replacedVerse = {};
+    					window.location.reload();
+    				}
+    			})
+    		}
+    	})
+    }
 
-  
-    replaceContentAndSave(){
-      let newContent;
-      let replaceCount;
-      let allChapterReplaceCount = [];
-      const searchValue = AutographaStore.searchValue;
-      const replaceValue = AutographaStore.replaceValue;
-      let oldContent = AutographaStore.translationContent;
 
-      this.findAndReplaceText(AutographaStore.searchValue, AutographaStore.replaceValue, AutographaStore.replaceOption);
-      AutographaStore.showModalSearch = false;
+    replaceContentAndSave() {
+    	let newContent;
+    	let replaceCount;
+    	let allChapterReplaceCount = [];
+    	const searchValue = AutographaStore.searchValue;
+    	const replaceValue = AutographaStore.replaceValue;
+		let oldContent = AutographaStore.translationContent;
+    	this.findAndReplaceText(AutographaStore.searchValue, AutographaStore.replaceValue, AutographaStore.replaceOption);
+    	AutographaStore.showModalSearch = false;
     }
 
 
@@ -207,7 +207,7 @@ class SearchModal extends React.Component {
         </Modal.Header>
         <Modal.Body>
           <FormGroup >
-            <RadioButtonGroup valueSelected="chapter" onChange={this.handleOption} name="SearchAndReplace" style={{display: "flex", marginBottom:"2%"}}>
+            <RadioButtonGroup valueSelected= {AutographaStore.replaceOption} onChange={(event, value) => {this.handleOption(value)}} name="SearchAndReplace" style={{display: "flex", marginBottom:"2%"}}>
               <RadioButton
                 value="chapter"
                 label={<FormattedMessage id="label-current-chapter" />}
@@ -225,14 +225,14 @@ class SearchModal extends React.Component {
             <TextField
               style={{marginTop: "-12px"}}
               hintText={<FormattedMessage id="placeholder-search-text" />}
-              onChange={this.handleFindChange.bind(this)}
+              onChange={this.handleFindChange}
             />
             <br />
             <label><FormattedMessage id="label-replace-with" /></label><br />
             <TextField
               style={{marginTop: "-12px"}}
               hintText={<FormattedMessage id="placeholder-replace-text" />}
-              onChange={this.handleReplaceChange.bind(this)}
+              onChange={this.handleReplaceChange}
             />
             <br />
           </div>
@@ -242,7 +242,8 @@ class SearchModal extends React.Component {
             style={{float: "right"}}
             label={<FormattedMessage id="btn-replace" />}
             primary={true}
-            onClick={this.replaceContentAndSave.bind(this)}
+			onClick={this.replaceContentAndSave.bind(this)}
+			disabled = {AutographaStore.searchValue === null || AutographaStore.searchValue === '' ? true : false}
           />
         </Modal.Footer>
       </Modal>
