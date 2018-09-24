2f1aa9e9d46cb9acc936788bc533996af2e9aeba
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-09-24 17:15:24 +0530

    Translation panel highlight bux fix

--- src/components/TranslationPanel.js
+++ src/components/TranslationPanel.js
@@ -14,58 +14,59 @@ import { FormattedMessage } from 'react-intl';
 
 @observer
 class TranslationPanel extends React.Component {
-  constructor(props){
-    super(props);
-    i18n.isRtl().then((res) => {
-      if(res) AutographaStore.scriptDirection = "rtl"
-    });
-    this.timeout =  0;
-  }
+	constructor(props){
+    	super(props);
+   		i18n.isRtl().then((res) => {
+      		if(res) AutographaStore.scriptDirection = "rtl"
+    	});
+   		this.timeout =  0;
+  	}
 
-  highlightRef(vId, refId, obj) {
-    {/*var content = ReactDOM.findDOMNode(this);
-    let verses = content.getElementsByClassName("verse-input")[0].querySelectorAll("span[id^=v]");
-    var refContent = document.getElementsByClassName('ref-contents');
-    for (var a=0; a< refContent.length; a++) {
-      var refContent2 = refContent[a];
-      for (var i = 0; i < AutographaStore.verses.length; i++) {
-        var refDiv = refContent2.querySelectorAll('div[data-verse^='+'"'+"r"+(i+1)+'"'+']');
-        if (refDiv != 'undefined') {
-          refDiv[0].style="background-color:none;font-weight:none;padding-left:10px;padding-right:10px";
-        }            
-      };
-      let chunk = document.getElementById(obj).getAttribute("data-chunk-group");
-      if (chunk) {
-        refContent2.querySelectorAll('div[data-verse^="r"]').style="background-color: '';font-weight: '';padding-left:10px;padding-right:10px";
-        var limits = chunk.split("-").map(function(element) { return parseInt(element, 10) - 1; });
-        for(var j=limits[0]; j<=limits[1];j++){
-          refContent2.querySelectorAll("div[data-verse=r"+(j+1)+"]")[0].style = "background-color: rgba(11, 130, 255, 0.1);padding-left:10px;padding-right:10px;margin-right:10px";
-        }
-        $('div[data-verse="r' + (limits[0] + 1) + '"]').css({ "border-radius": "10px 10px 0px 0px" });
-        $('div[data-verse="r' + (limits[1] + 1) + '"]').css({ "border-radius": "0px 0px 10px 10px" });
-      }
-    }*/}
-      let refContent = document.getElementsByClassName('ref-contents');
-      for(let l=0; l<AutographaStore.layout; l++){
-        let ref = refContent[l].querySelectorAll('div[data-verse^="r"]')
-        for (let i=0; i < ref.length; i++) {
-          if (ref[i] != 'undefined') {
-            ref[i].style="background-color:none;font-weight:none;padding-left:10px;padding-right:10px";
-          }
-        };
-        refContent[l].querySelectorAll('div[data-verse^='+'"'+"r"+(refId+1)+'"'+']')[0].style = "background-color: rgba(11, 130, 255, 0.1);padding-left:10px;padding-right:10px;border-radius: 10px";
-      }
-  }
+  	highlightRef(vId, refId, obj) {
+		{/*var content = ReactDOM.findDOMNode(this);
+		let verses = content.getElementsByClassName("verse-input")[0].querySelectorAll("span[id^=v]");
+		var refContent = document.getElementsByClassName('ref-contents');
+		for (var a=0; a< refContent.length; a++) {
+		var refContent2 = refContent[a];
+		for (var i = 0; i < AutographaStore.verses.length; i++) {
+			var refDiv = refContent2.querySelectorAll('div[data-verse^='+'"'+"r"+(i+1)+'"'+']');
+			if (refDiv != 'undefined') {
+			refDiv[0].style="background-color:none;font-weight:none;padding-left:10px;padding-right:10px";
+			}            
+		};
+		let chunk = document.getElementById(obj).getAttribute("data-chunk-group");
+		if (chunk) {
+			refContent2.querySelectorAll('div[data-verse^="r"]').style="background-color: '';font-weight: '';padding-left:10px;padding-right:10px";
+			var limits = chunk.split("-").map(function(element) { return parseInt(element, 10) - 1; });
+			for(var j=limits[0]; j<=limits[1];j++){
+			refContent2.querySelectorAll("div[data-verse=r"+(j+1)+"]")[0].style = "background-color: rgba(11, 130, 255, 0.1);padding-left:10px;padding-right:10px;margin-right:10px";
+			}
+			$('div[data-verse="r' + (limits[0] + 1) + '"]').css({ "border-radius": "10px 10px 0px 0px" });
+			$('div[data-verse="r' + (limits[1] + 1) + '"]').css({ "border-radius": "0px 0px 10px 10px" });
+		}
+		}*/}
+      	let refContent = document.getElementsByClassName('ref-contents');
+      	for(let l=0; l<AutographaStore.layout; l++){
+        	let ref = refContent[l] ? refContent[l].querySelectorAll('div[data-verse^="r"]') : [];
+        	for (let i=0; i < ref.length; i++) {
+          		if (ref[i] != 'undefined') {
+            		ref[i].style="background-color:none;font-weight:none;padding-left:10px;padding-right:10px";
+          		}
+        	};
+        	if( refContent[l])
+          		refContent[l].querySelectorAll('div[data-verse^='+'"'+"r"+(refId+1)+'"'+']')[0].style = "background-color: rgba(11, 130, 255, 0.1);padding-left:10px;padding-right:10px;border-radius: 10px";
+      	}
+  	}
 
-  handleKeyUp =(e)=> {
-    if(this.timeout) clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      if(!AutographaStore.setDiff){
-        this.props.onSave();
-      }
-    }, 3000);
-  }
-  openStatPopup =() => {
+	handleKeyUp =(e)=> {
+		if(this.timeout) clearTimeout(this.timeout);
+			this.timeout = setTimeout(() => {
+				if(!AutographaStore.setDiff){
+					this.props.onSave();
+				}
+			}, 3000);
+	}
+  	openStatPopup =() => {
         this.showReport();
         AutographaStore.showModalStat = true
     }
@@ -108,36 +109,36 @@ class TranslationPanel extends React.Component {
         })  
     }
   
-  render (){
-    let verseGroup = [];
-    const toggle = AutographaStore.toggle;
+  	render (){
+    	let verseGroup = [];
+    	const toggle = AutographaStore.toggle;
 
-    for (let i = 0; i < AutographaStore.chunkGroup.length; i++) {
-      let vid="v"+(i+1);
-      verseGroup.push(<div key={i} id={`versediv${i+1}`} onClick={this.highlightRef.bind(this, vid, i)}>
-          <span className='verse-num' key={i}>{(i+1)}</span>
-          <span contentEditable={true} suppressContentEditableWarning={true} id={vid} data-chunk-group={AutographaStore.chunkGroup[i]} onKeyUp={this.handleKeyUp}>
-          {AutographaStore.translationContent[i]}
-          </span>
-        </div>
-      ); 
-    }
-    const {tIns, tDel} = this.props;
-    return (
-      <div className="col-editor container-fluid">
-        <div className="row">
-          <div className="col-12 center-align">
-              <p className="translation"><a href="javscript:;" style = {{fontWeight: "bold", pointerEvents: toggle ? "none" : "" }} onClick={() => this.openStatPopup()}><FormattedMessage id="label-translation" /></a></p>
-          </div>
-        </div>
-        <div className="row">
-          {tIns || tDel ? <div style={{textAlign: "center"}}><span style={{color: '#27b97e', fontWeight: 'bold'}}>(+) <span id="tIns">{tIns}</span></span> | <span style={{color: '#f50808', fontWeight: 'bold'}}> (-) <span id="tDel">{tDel}</span></span></div> : "" }
-          <div id="input-verses" className={`col-12 col-ref verse-input ${AutographaStore.scriptDirection.toLowerCase()} ${tIns || tDel ? 'disable-input' : ''}`} dir={AutographaStore.scriptDirection} style={{pointerEvents: tIns || tDel ? 'none': ''}}>{verseGroup}</div>
-        </div>
-        <Statistic show={AutographaStore.showModalStat}  showReport = {this.showReport}/>
-      </div>
-    ) 
-  }
+		for (let i = 0; i < AutographaStore.chunkGroup.length; i++) {
+		let vid="v"+(i+1);
+		verseGroup.push(<div key={i} id={`versediv${i+1}`} onClick={this.highlightRef.bind(this, vid, i)}>
+			<span className='verse-num' key={i}>{(i+1)}</span>
+			<span contentEditable={true} suppressContentEditableWarning={true} id={vid} data-chunk-group={AutographaStore.chunkGroup[i]} onKeyUp={this.handleKeyUp}>
+			{AutographaStore.translationContent[i]}
+			</span>
+			</div>
+		); 
+		}
+		const {tIns, tDel} = this.props;
+		return (
+			<div className="col-editor container-fluid">
+				<div className="row">
+				<div className="col-12 center-align">
+					<p className="translation"><a href="javscript:;" style = {{fontWeight: "bold", pointerEvents: toggle ? "none" : "" }} onClick={() => this.openStatPopup()}><FormattedMessage id="label-translation" /></a></p>
+				</div>
+				</div>
+				<div className="row">
+				{tIns || tDel ? <div style={{textAlign: "center"}}><span style={{color: '#27b97e', fontWeight: 'bold'}}>(+) <span id="tIns">{tIns}</span></span> | <span style={{color: '#f50808', fontWeight: 'bold'}}> (-) <span id="tDel">{tDel}</span></span></div> : "" }
+				<div id="input-verses" className={`col-12 col-ref verse-input ${AutographaStore.scriptDirection.toLowerCase()} ${tIns || tDel ? 'disable-input' : ''}`} dir={AutographaStore.scriptDirection} style={{pointerEvents: tIns || tDel ? 'none': ''}}>{verseGroup}</div>
+				</div>
+				<Statistic show={AutographaStore.showModalStat}  showReport = {this.showReport}/>
+			</div>
+		) 
+  	}
 }
 
 module.exports = TranslationPanel;
