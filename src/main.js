cd3e54df3747ee20bf96126858954d91728ad7b8
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-10-10 10:46:58 +0530

    eletron debug change

--- src/main.js
+++ src/main.js
@@ -2,38 +2,44 @@ const electron = require('electron');
 const session = require('electron').session;
 
 // Module to control application life.
-const {app} = electron
+const { app } = electron
 // Module to create native browser window.
-const {BrowserWindow} = electron;
+const { BrowserWindow } = electron;
 import path from 'path';
 import url from 'url';
 import env from 'env';
 
-import {existsSync, mkdirSync} from 'fs'
+import { existsSync, mkdirSync } from 'fs'
 
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
+// Allow F12 or Ctrl-Shift-I to open Chrome Dev Tools when in development mode.
+if (env.name === "development") {
+    require('electron-debug')({
+        showDevTools: false,
+        devToolsMode: "previous"
+    });
+}
+
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
-    icon:'/assets/images/logo.png',
-    width: 800,
-    height: 600,
-     height: 600,
-    'min-width': 600,
-    'min-height': 300,
-    'accept-first-mouse': true,
-    'title-bar-style': 'hidden',
-    'webPreferences': {'session': session},
-    show: false
+        icon: '/assets/images/logo.png',
+        width: 800,
+        height: 600,
+        height: 600,
+        'min-width': 600,
+        'min-height': 300,
+        'accept-first-mouse': true,
+        'title-bar-style': 'hidden',
+        'webPreferences': { 'session': session },
+        show: false
     });
-    if (env.name === "development") {
-        //win.openDevTools();
-    }
-    
+   
+
     // and load the index.html of the app.
     // win.loadURL(`file:${__dirname}/views/index.html`);
     win.setMenu(null);
@@ -42,72 +48,68 @@ function createWindow() {
     // pathname: path.join(__dirname, '/views/index.html'),
     // slashes:  true}))
     win.loadURL(
-         url.format({
-      pathname: path.join(__dirname, "/views/index.html"),
-      protocol: "file:",
-      slashes: true
-    })
+        url.format({
+            pathname: path.join(__dirname, "/views/index.html"),
+            protocol: "file:",
+            slashes: true
+        })
     )
 
     //loading window gracefully
     win.once('ready-to-show', () => {
-	// Open the DevTools.
-	//win.webContents.openDevTools();	
-	win.maximize();
+        win.maximize();
         win.show();
     });
 
     // Emitted when the window is closed.
     win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-	win = null;
-	if (process.platform !== 'darwin') {
-	    app.quit();
-	}
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
     });
 }
 
 var dbSetup = new Promise(
-  function (resolve, reject) {
-    if ( !existsSync('db') ) {
-      mkdirSync( 'db' );
-    }
-	// Setup database.
-	var dbUtil = require(`${__dirname}/util/DbUtil.js`);
-	dbUtil.setupTargetDb
-	    .then((response) => {
-		console.log(response);
-		return dbUtil.setupRefDb;
-	    })
-	    .then((response) => {
-		console.log(response);
-        return dbUtil.setupLookupsDb;
-	    })
-        .then((response)=>{
-            console.log(response)
-            resolve(response)
-        })
-        .catch((err) => {
-		console.log('Error while DB setup. ' + err);
-		reject(err);
-	    });
+    function (resolve, reject) {
+        if (!existsSync('db')) {
+            mkdirSync('db');
+        }
+        // Setup database.
+        var dbUtil = require(`${__dirname}/util/DbUtil.js`);
+        dbUtil.setupTargetDb
+            .then((response) => {
+                console.log(response);
+                return dbUtil.setupRefDb;
+            })
+            .then((response) => {
+                console.log(response);
+                return dbUtil.setupLookupsDb;
+            })
+            .then((response) => {
+                console.log(response)
+                resolve(response)
+            })
+            .catch((err) => {
+                console.log('Error while DB setup. ' + err);
+                reject(err);
+            });
     });
 
 function preProcess() {
     dbSetup
-	.then((response) => {
-	    createWindow();
-	})
-	.catch((err) => {
-	    console.log('Error while App intialization.' + err);
-	});
+        .then((response) => {
+            createWindow();
+        })
+        .catch((err) => {
+            console.log('Error while App intialization.' + err);
+        });
 }
 
-// if (env.name === "development") {
-//     win.openDevTools();
-//   }
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -119,7 +121,7 @@ app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-    app.quit();
+        app.quit();
     }
 });
 
@@ -127,10 +129,10 @@ app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-    createWindow();
+        createWindow();
     }
     // win.openDevTools();
-    
+
 });
 
 //code for sigle instance at a time according to electron 3.0.0
