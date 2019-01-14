d24359f0fa2bfa75862858d0a4b55c0305f30d9b
Sandeep Kumar <sandeepkumar0486@gmail.com>
2019-01-14 17:20:42 +0530

    export all code modify

--- src/components/Download.js
+++ src/components/Download.js
@@ -30,54 +30,44 @@ class DownloadModal extends React.Component {
     clickStage = (e) => {
         this.setState({ buttonStage: "label-stage", stageChange: e, stageName: '' });
     }
-    
-    exportUsfm = (e) => {
+
+    exportAllUsfm = async(e,books) => {
+        AutographaStore.showModalDownload = true;
         const {stageChange, stageName} = this.state;
         let stageInput = stageName ? stageName : `Stage ${stageChange}`;
+        const currentTrans = AutographaStore.currentTrans;
         let book = {};
-        const currentTrans = AutographaStore.currentTrans;
-        db.get('targetBible').then(function(doc) {
-            book.bookNumber = AutographaStore.bookId.toString();
-            book.bookName = constants.booksList[parseInt(book.bookNumber, 10) - 1];
-            book.bookCode = constants.bookCodeList[parseInt(book.bookNumber, 10) - 1];
-            book.outputPath = doc.targetPath;
-            let filepath = bibUtil.toUsfm(book, stageInput, doc);
-            return filepath;
-        }).then((filepath) => {
-            AutographaStore.showModalDownload = false;
-            swal({title: currentTrans["tooltip-export-usfm"], text: `${currentTrans["label-exported-file"]}:${filepath}`})
-        }).catch((err) => {
-            console.log(err)
-            AutographaStore.showModalDownload = false;
+        let filepath;
+        let doc = await db.get('targetBible');
+        try {
+            if(books === 'current') {
+                book.bookNumber = AutographaStore.bookId.toString();
+                book.bookName = constants.booksList[parseInt(book.bookNumber, 10) - 1];
+                book.bookCode = constants.bookCodeList[parseInt(book.bookNumber, 10) - 1];
+                book.outputPath = doc.targetPath;
+                filepath = bibUtil.toUsfm(book, stageInput, doc);
+                AutographaStore.showModalDownload = false;
+                swal({title: currentTrans["tooltip-export-usfm"], text: `${currentTrans["label-exported-file"]}:${filepath}`})
+            }
+            if(books === '*') {
+                constants.bookCodeList.forEach((value, index) => {
+                    book={};
+                    book.bookNumber = (index+1).toString();
+                    book.bookName = constants.booksList[index];
+                    book.bookCode = value;
+                    book.outputPath = doc.targetPath;
+                    filepath = bibUtil.toUsfm(book, stageInput, doc);
+                    AutographaStore.showModalDownload = false;
+                    swal({title: currentTrans["tooltip-backup-usfm"]})
+                })
+            }
+        }
+        catch(ex) {
             swal(currentTrans["dynamic-msg-error"], currentTrans["dynamic-msg-enter-translation"])
-        });
-    }
-
-    exportAllUsfm = (e,flag) => {
-        const {stageChange, stageName} = this.state;
-        let stageInput = stageName ? stageName : `Stage ${stageChange}`;
-        const currentTrans = AutographaStore.currentTrans;
-
-        for(let i =1; i<= Number(flag); i++){
-            let book = {};
-        db.get('targetBible').then(function(doc) {
-            book.bookNumber = i.toString();
-            if(flag===1)
-            book.bookNumber = AutographaStore.bookId.toString();
-            book.bookName = constants.booksList[parseInt(book.bookNumber, 10) -1];
-            book.bookCode = constants.bookCodeList[parseInt(book.bookNumber, 10) -1];
-            book.outputPath = doc.targetPath;
-            let filepath = bibUtil.toUsfm(book, stageInput, doc);
-            return filepath;
-        }).then((filepath) => {
+        }
+        finally {
             AutographaStore.showModalDownload = false;
-            swal({title: currentTrans["tooltip-export-usfm"], text: `${currentTrans["label-exported-file"]}:${filepath}`})
-        }).catch((err) => {
-            console.log(err)
-            AutographaStore.showModalDownload = false;
-            swal(currentTrans["dynamic-msg-error"], currentTrans["dynamic-msg-enter-translation"])
-        });
-    }
+        }
     }
 
     render (){
@@ -145,12 +135,12 @@ class DownloadModal extends React.Component {
                 <Modal.Footer>
                 <FormattedMessage id="btn-export">
                 {(message) =>
-                  <RaisedButton style={{float: "right", marginLeft:"5px"}} disabled={stageName || stageChange ? false : true} id="btn-export-usfm" label={message} primary={true} onClick={(e) => this.exportAllUsfm(e,1)}/>
+                  <RaisedButton style={{float: "right", marginLeft:"5px"}} disabled={stageName || stageChange ? false : true} id="btn-export-usfm" label={message} primary={true} onClick={(e) => this.exportAllUsfm(e, 'current')}/>
                 }
                 </FormattedMessage>
                 <FormattedMessage id="btn-export-all">
                 {(message) =>
-                  <RaisedButton style={{float: "right"}} disabled={stageName || stageChange ? false : true} id="btn-export-usfm" label={message} primary={true} onClick={(e) => this.exportAllUsfm(e,66)}/>
+                  <RaisedButton style={{float: "right"}} disabled={stageName || stageChange ? false : true} id="btn-export-usfm" label={message} primary={true} onClick={(e) => this.exportAllUsfm(e, '*')}/>
                 }
                 </FormattedMessage>
                 </Modal.Footer>
