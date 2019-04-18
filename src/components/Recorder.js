b757c01924d66ae9c77d8f9519538757e8deb650
Ashersam <ashersamuel06@gmail.com>
2019-04-18 16:38:06 +0530

    cleaning code

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -9,12 +9,7 @@ import AudioPlayer from "react-h5-audio-player";
 const db = require(`${__dirname}/../util/data-provider`).targetDb();
 const constants = require("../util/constants");
 let saveRec = require("../util/Rec_save_exp");
-
-// import Fab from '@material-ui/core/Fab';
-// import Icon from '@material-ui/core/Icon';
-
-// import {version} from "../../package.json";
-var audio, file, savedfile = [], filesave;;
+var file, savedfile = [];
 
 @observer
 class RecorderModal extends React.Component {
@@ -35,12 +30,10 @@ class RecorderModal extends React.Component {
     });
   };
 
-  onStop = async(recordedBlob) => {
+  onStop = async recordedBlob => {
     console.log("recordedBlob is: ", recordedBlob);
-    // audio = new Audio(recordedBlob.blobURL);
     file = recordedBlob;
     let book = {};
-    // var date = new Date();
     let doc = await db.get("targetBible");
     doc = doc.targetPath;
     book.bookNumber = AutographaStore.bookId.toString();
@@ -52,16 +45,17 @@ class RecorderModal extends React.Component {
   closeRecorder = () => {
     AutographaStore.showModalRecorder = false;
   };
-  playrecorder = (key) => {
+  playrecorder = key => {
     AutographaStore.showModalPlayer = true;
-    this.setState({playSrc: savedfile[key].blobURL});
+    this.setState({ playSrc: savedfile[key].blobURL });
   };
   closePlayer = () => {
     AutographaStore.showModalPlayer = false;
-  }
+  };
 
-  saveRecorder = async(key) => {
+  saveRecorder = async key => {
     let book = {};
+    let filesave;
     const currentTrans = AutographaStore.currentTrans;
     let doc = await db.get("targetBible");
     doc = doc.targetPath;
@@ -78,106 +72,103 @@ class RecorderModal extends React.Component {
 
   render() {
     return (
-    <div>
-      <Modal
-        show={AutographaStore.showModalRecorder}
-        onHide={this.closeRecorder}
-        className="RecTab"
-        id="tab-record"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <FormattedMessage id="tooltip-recorder" />
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div>
-            <ReactMicPlus
-              record={this.state.record}
-              className="sound-wave"
-              onStop={this.onStop}
-              strokeColor="#000000"
-              backgroundColor="#0069D6"
-              nonstop={true}
-            />
-            <RaisedButton
-              buttonStyle={{ borderRadius: 25 }}
-              style={{ borderRadius: 25, marginLeft: "30px" }}
-              onClick={this.startRecording}
-            >
-              <i className="fa fa-microphone" />
-            </RaisedButton>
-            <RaisedButton
-              buttonStyle={{ borderRadius: 25 }}
-              style={{ borderRadius: 25 }}
-              backgroundColor={"white"}
-              onClick={this.stopRecording}
-            >
-              <i className="fa fa-stop" />
-            </RaisedButton>
-            <RaisedButton
-              buttonStyle={{ borderRadius: 25 }}
-              style={{ borderRadius: 25 }}
-              backgroundColor={"white"}
-              onClick={this.playrecorder}
-            >
-              <i className="fa fa-play-circle" />
-            </RaisedButton>
-          </div>
-          <div className="savelist">
-            {savedfile.map((file, key) => (
-              <span
-                id={key}
-                key={key}
-                style={{
-                  width: "550px",
-                  display: "inline-block"
+      <div>
+        <Modal
+          show={AutographaStore.showModalRecorder}
+          onHide={this.closeRecorder}
+          className="RecTab"
+          id="tab-record"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <FormattedMessage id="tooltip-recorder" />
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <ReactMicPlus
+                record={this.state.record}
+                className="sound-wave"
+                onStop={this.onStop}
+                strokeColor="#000000"
+                backgroundColor="#0069D6"
+                nonstop={true}
+              />
+              <RaisedButton
+                buttonStyle={{ borderRadius: 25 }}
+                style={{ borderRadius: 25, marginLeft: "30px" }}
+                onClick={this.startRecording}
+              >
+                <i className="fa fa-microphone" />
+              </RaisedButton>
+              <RaisedButton
+                buttonStyle={{ borderRadius: 25 }}
+                style={{ borderRadius: 25 }}
+                backgroundColor={"white"}
+                onClick={this.stopRecording}
+              >
+                <i className="fa fa-stop" />
+              </RaisedButton>
+              <RaisedButton
+                buttonStyle={{ borderRadius: 25 }}
+                style={{ borderRadius: 25 }}
+                backgroundColor={"white"}
+                onClick={this.playrecorder}
+              >
+                <i className="fa fa-play-circle" />
+              </RaisedButton>
+            </div>
+            <div className="savelist">
+              {savedfile.map((file, key) => (
+                <span
+                  id={key}
+                  key={key}
+                  style={{
+                    width: "550px",
+                    display: "inline-block"
                   }}
                 >
-                <ul>{file.bookName}
-                {/* <AudioPlayer
-                src={file.blobURL}
-                onPlay={e => console.log("onPlay")}
-                ></AudioPlayer> */}
-                <RaisedButton
-                buttonStyle={{ borderRadius: 25 }}
-                className="saveRec"
-                style={{ borderRadius: 25 }}
-                backgroundColor={"white"}
-                onClick={ () => this.saveRecorder(key)}
-                >
-                <i className="fa fa-floppy-o"/>
-                </RaisedButton>
-                <RaisedButton
-                buttonStyle={{ borderRadius: 25 }}
-                className="playRec"
-                style={{ borderRadius: 25 }}
-                backgroundColor={"white"}
-                onClick={() => this.playrecorder(key)}
-                >
-                <i className="fa fa-play-circle" />
-                </RaisedButton>
-                <Modal
-                show={AutographaStore.showModalPlayer}
-                onHide={this.closePlayer}
-                className="playerModel"
-                >
-                <Modal.Header closeButton>
-                <Modal.Title>Player</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                <AudioPlayer
-                src={this.state.playSrc}
-                onPlay={ (e) => console.log("onPlay")}>
-                </AudioPlayer>
-                 </Modal.Body>
-             </Modal>
-             </ul>
-            </span>
-            ))}
-          </div>
-        </Modal.Body>
-      </Modal>
+                  <ul>
+                    {file.bookName}
+                    <RaisedButton
+                      buttonStyle={{ borderRadius: 25 }}
+                      className="saveRec"
+                      style={{ borderRadius: 25 }}
+                      backgroundColor={"white"}
+                      onClick={() => this.saveRecorder(key)}
+                    >
+                      <i className="fa fa-floppy-o" />
+                    </RaisedButton>
+                    <RaisedButton
+                      buttonStyle={{ borderRadius: 25 }}
+                      className="playRec"
+                      style={{ borderRadius: 25 }}
+                      backgroundColor={"white"}
+                      onClick={() => this.playrecorder(key)}
+                    >
+                      <i className="fa fa-play-circle" />
+                    </RaisedButton>
+                    <Modal
+                      show={AutographaStore.showModalPlayer}
+                      onHide={this.closePlayer}
+                      className="playerModel"
+                    >
+                      <Modal.Header closeButton>
+                        <Modal.Title>Player</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>
+                        <AudioPlayer
+                          src={this.state.playSrc}
+                          onPlay={e => console.log("onPlay")}
+                        />
+                      </Modal.Body>
+                    </Modal>
+                  </ul>
+                </span>
+              ))}
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     );
   }
