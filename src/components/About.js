3bfcd739744b479838d39969e1be69871efd20f8
Ashersam <ashersamuel06@gmail.com>
2019-06-11 07:18:27 +0100

    license changed

--- src/components/About.js
+++ src/components/About.js
@@ -31,7 +31,7 @@ var AboutUsModel = function(props) {
                 <Tab eventKey={2} title={<FormattedMessage id="label-license-tab"/>}>
                     <div style={{overflowY: "scroll", height: "255px"}}>
                         <h4> The MIT License (MIT)</h4>
-                        <p>Released in 2018 by Friends of Agape (www.friendsofagape.org) in partnership with RUN Ministries (www.runministries.org). </p>
+                        <p>Copyright (c) 2019 Friends of Agape</p>
                         <br />
                         <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
                         <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
